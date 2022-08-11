b22a957b1f673a1769ba5872a4ce8ccddb9180af
yuemillo <yc.gd1688@gmail.com>
2022-08-11 22:05:34 +0800

    Update 2022-08-11T14:05:29.621Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,4 +4,8 @@ console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
 });
+$('.ham-icon').click(function (e) {
+  e.preventDefault();
+  $('.dropdown').toggleClass('active');
+});
 //# sourceMappingURL=all.js.map
