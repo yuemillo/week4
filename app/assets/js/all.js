96ee1ca41713cb81922f97f90119e8aecc4c093f
yuemillo <yc.gd1688@gmail.com>
2022-08-11 22:07:59 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,3 +3,8 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HesSchool Hello!');
 });
+
+$('.ham-icon').click(function(e) {
+  e.preventDefault();
+  $('.dropdown').toggleClass('active')
+})
